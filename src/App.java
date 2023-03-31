e7acba0f7eb4e19a4a56959da8aad35165095415
Erikcwill <erikcwill@gmail.com>
2023-03-30 21:32:53 -0300

    Creating of a StickerGenerator Class and updating the App class to use it.

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -11,21 +13,32 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         // fazer uma conexão HTTP e buscar filmes
-        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        //String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopTVs.json";  
+        //String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";      
+        //String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularTVs.json";        
         URI adress = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(adress).GET().build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        HttpResponse < String > response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
 
         // extrair só os dados que interessam (titulo, poster, classificação)
         var parser = new JsonParser();
-        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        List < Map < String, String >> listaDeFilmes = parser.parse(body);
 
         // exibir e manipular os dados
-        for (int i = 0; i < 3; i++) {
-            Map<String, String> filme = listaDeFilmes.get(i);
-            
+        for (int i = 0; i < 10; i++) {
+            Map < String, String > filme = listaDeFilmes.get(i);
+
+            String urlImage = filme.get("image");
+            String title = filme.get("title");
+            InputStream inputStream = new URL(urlImage).openStream();
+            String fileName = "output/" + title + ".png";
+
+            var generator = new StickerGenerator();
+            generator.create(inputStream, fileName);
+
             // Título
             System.out.print("\u001b[1m");
             System.out.print("╭");
@@ -33,7 +46,7 @@ public class App {
                 System.out.print("─");
             }
             System.out.println("╮");
-            System.out.println("│ " + filme.get("title") + " │");
+            System.out.println("│ " + title + " │");
             System.out.print("╰");
             for (int j = 0; j < filme.get("title").length() + 2; j++) {
                 System.out.print("─");
@@ -42,7 +55,7 @@ public class App {
 
             // URL da imagem
             System.out.print("\u001b[3m URL da imagem: \u001b[0m");
-            System.out.println(filme.get("image"));
+            System.out.println(urlImage);
 
             // Classificação
             double classificacao = Double.parseDouble(filme.get("imDbRating"));
---
+++ src/StickerGenerator.java
@@ -0,0 +1,41 @@
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
+public class StickerGenerator {
+
+    public void create(InputStream inputStream, String fileName) throws Exception {
+
+        // Image read
+        // InputStream inputStream = new FileInputStream(new
+        // File("input/movie-big.jpg"));
+        // InputStream inputStream = new
+        // URL("https://m.media-amazon.com/images/I/71715eBi1sL._AC_SL1000_.jpg").openStream();
+        BufferedImage originalImage = ImageIO.read(inputStream);
+        int width = originalImage.getWidth();
+        int height = originalImage.getHeight();
+        int newHeight = height + 200;
+        BufferedImage newImage = new BufferedImage(width, newHeight, BufferedImage.TRANSLUCENT);
+
+        Graphics2D graphics = (Graphics2D) newImage.getGraphics();
+        graphics.drawImage(originalImage, 0, 0, null);
+
+        File directory = new File("output");
+        if (!directory.exists()) {
+            directory.mkdir();
+        }
+
+        var font = new Font(Font.SANS_SERIF, Font.BOLD, 100);
+        graphics.setFont(font);
+
+        graphics.drawString("TOPZERA", 0, newHeight - 100);
+
+        ImageIO.write(newImage, "png", new File(fileName));
+
+    }
+
+}
